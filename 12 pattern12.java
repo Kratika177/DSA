b090b7eaa59393a1083854871d3a773333684c67
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2023-11-23 00:17:16 +0530

    Create 12 pattern12.java

---
+++ 12 pattern12.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static void numberCrown(int n) {
+        // Write your code here.
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int l=i;l>=1;l--){
+                System.out.print(l+" ");
+            }
+            System.out.println();
+        }
+    }
+}
