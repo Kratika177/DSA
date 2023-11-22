e1fc77c3fd7f444b726446c077b53c164b6091bd
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2023-11-23 00:35:13 +0530

    Create 15 pattern15.java

---
+++ 15 pattern15.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void nLetterTriangle(int n) {
+        // Write your code here
+        for(int i=65+n-1;i>=65;i--){
+            for(int j=65;j<=i;j++){
+                System.out.print((char)j+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
