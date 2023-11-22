74b461e54de7213b27f8ffcfdcdd031057ff616b
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2023-11-23 00:04:25 +0530

    Create 11 pattern11.java

---
+++ 11 pattern11.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public static void nBinaryTriangle(int n) {
+        // Write your code here.
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                for(int j=1;j<=i;j++){
+                    if(j%2==0){
+                        System.out.print("0 ");
+                    }
+                    else{
+                        System.out.print("1 ");
+                    }
+                }
+            }
+            else{
+                for(int j=1;j<=i;j++){
+                    if(j%2!=0){
+                        System.out.print("0 ");
+                    }
+                    else{
+                        System.out.print("1 ");
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
